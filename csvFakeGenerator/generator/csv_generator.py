f9b7e2f56ce6743e1ebb811d3159155846987046
Alex Abyzov <82107400+S-P-ring@users.noreply.github.com>
2023-03-10 12:52:47 +0200

    Update csv_generator.py

--- csvFakeGenerator/generator/csv_generator.py
+++ csvFakeGenerator/generator/csv_generator.py
@@ -4,8 +4,11 @@ import random
 
 import uuid
 
+from .models import DataSchema
 
-def prepare_data(object):
+
+def prepare_data(object: DataSchema) -> List[str]:
+    """Accepts a model object and returns a list of column names"""
     fields = {}
     for column in object.column_set.all():
         if column.data_type in ('TEXT', 'INTEGER'):
@@ -17,12 +20,12 @@ def prepare_data(object):
     return fields
 
 
-def generate_filename(extension):
+def generate_filename(extension: str) -> str:
     unique_filename = str(uuid.uuid4())
     return f"{unique_filename}.{extension}"
 
 
-def generate_data(data_type, range_min, range_max):
+def generate_data(data_type: str, range_min: int, range_max: int) -> str | int:
     fake = Faker()
     if data_type == 'TEXT':
         return fake.paragraph(nb_sentences=random.randint(range_min, range_max))
@@ -48,7 +51,7 @@ def generate_data(data_type, range_min, range_max):
         return None
 
 
-def generate_row(field_dict):
+def generate_row(field_dict:dict) -> List[str]:
     row_data = []
     for field_name in sorted(field_dict.keys(), key=lambda x: field_dict[x]['order']):
         field_data = field_dict[field_name]
@@ -62,7 +65,8 @@ def generate_row(field_dict):
     return row_data
 
 
-def generate_csv(field_dict, num_rows, column_separator, string_character):
+def generate_csv(field_dict: dict, num_rows: str, column_separator: str, string_character: str) -> None:
+    num_rows = int(num_rows)
     filename = generate_filename('csv')
     with open(filename, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=column_separator, quotechar=string_character)
