ded688a67ed0714124c8cb12dc76ccf7679881f8
Alex Abyzov <82107400+S-P-ring@users.noreply.github.com>
2023-03-10 13:09:42 +0200

    Update services.py

--- csvFakeGenerator/generator/services.py
+++ csvFakeGenerator/generator/services.py
@@ -11,3 +11,21 @@ def get_schema(pk):
 
 def create_dataset(data_schema):
     return DataSet.objects.create(data_schema=data_schema)
+
+def get_ajax_data_response(data_set):
+    data = {
+            'status': 'success',
+            'dataset_pk': data_set.pk,
+            'dataset_time_create': data_set.created_date, }
+    return data    
+
+def generate_data(*, schema_pk, data_set, num_records):
+        data_schema = get_schema(schema_pk)
+        fields = prepare_data(data_schema)
+        data_set.status = 'In progress'
+        column_separator = data_set.column_separator
+        string_character = data_set.string_character
+        generate_csv(fields, num_records, column_separator, string_character)
+        
+ def get_num_records(request):
+     retunr int(request.POST.get('num_records'))
